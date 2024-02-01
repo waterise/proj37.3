d9d6596c20d7508ab464a50a7866752937f1898e
ITSTEP\st <st>
2024-02-01 18:20:55 +0300

    added sum function

---
+++ proj37.3/Source.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+int count(int a, int b);
+int main() {
+	int n = 4, m = 9;
+	cout << count(n, m);
+	return 0;
+}
---
+++ proj37.3/Source1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int count(int a, int b) {
+	a += b;
+
+	return a;
+}
